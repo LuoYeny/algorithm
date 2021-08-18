9b4b2f9825675a489d6588184c762071ab0eb642
Luoyeny <934398274@qq.com>
2021-08-19 00:08:25 +0800

    2-4

--- src/com/ch1/sort/test/RebaseTest.java
+++ src/com/ch1/sort/test/RebaseTest.java
@@ -3,7 +3,7 @@ package com.ch1.sort.test;
 public class RebaseTest {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
+        System.out.println("4");
         System.out.println("3");
     }
 }
