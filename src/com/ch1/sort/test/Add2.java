56bb69316cfa17c425164a5c6913e23ef61c691f
Luoyeny <934398274@qq.com>
2021-08-18 21:49:20 +0800

    add add2

--- src/com/ch1/sort/test/Add2.java
+++ src/com/ch1/sort/test/Add2.java
@@ -2,6 +2,15 @@ package com.ch1.sort.test;
 
 public class Add2 {
     public static void main(String[] args) {
+
         System.out.println("sdd");
+
+        System.out.println("123");
+        main1("123");
+    }
+
+    public static void main1(String args) {
+
+
     }
 }
